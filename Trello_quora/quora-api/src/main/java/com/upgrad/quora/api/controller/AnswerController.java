70f6c1c04b83221eaebb6bb8e7a4ef68f33dcdc7
aakashmt97 <64053786+aakashmt97@users.noreply.github.com>
2021-03-28 16:33:11 +0530

    Formatting changes

--- Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
+++ Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +23,7 @@ public class AnswerController {
 
     @Autowired
     private AnswerService answerService;
+    
     //This endpoint is used to create an answer to a particular question. Any user can access this endpoint.
     @RequestMapping(method = RequestMethod.POST, path = "/question/{questionId}/answer/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<AnswerResponse> createAnswer(@PathVariable("questionId") final String questionId, final AnswerRequest answerRequest, @RequestHeader("authorization") final String authorization) throws AuthorizationFailedException, InvalidQuestionException {
@@ -32,11 +32,11 @@ public class AnswerController {
         answerEntity.setDate(ZonedDateTime.now());
         answerEntity.setAnswer(answerRequest.getAnswer());
 
-
         final AnswerEntity createdAnswerEntity = answerService.createAnswer(answerEntity, authorization, questionId);
         AnswerResponse answerResponse = new AnswerResponse().id(createdAnswerEntity.getUuid()).status("Answer Created");
         return new ResponseEntity<AnswerResponse>(answerResponse, HttpStatus.CREATED);
     }
+    
     //This endpoint is used to edit an answer. Only the owner of the answer can edit the answer.
     @RequestMapping(method = RequestMethod.PUT, path = "/answer/edit/{answerId}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<AnswerEditResponse> editAnswer(final AnswerEditRequest answerEditRequest, @PathVariable("answerId") final String answerId, @RequestHeader("authorization") final String authorization) throws AuthorizationFailedException, AnswerNotFoundException {
@@ -45,11 +45,11 @@ public class AnswerController {
         answerEntity.setUuid(answerId);
         answerEntity.setDate(ZonedDateTime.now());
 
-
         final AnswerEntity updatedAnswerEntity = answerService.editAnswer(answerEntity, authorization, answerId);
         AnswerEditResponse answerEditResponse = new AnswerEditResponse().id(updatedAnswerEntity.getUuid()).status("Answer Edited");
         return new ResponseEntity<AnswerEditResponse>(answerEditResponse, HttpStatus.OK);
     }
+    
     //This endpoint is used to delete an answer. Only the owner of the answer or admin can delete an answer.
     @RequestMapping(path = "/answer/delete/{answerId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<AnswerDeleteResponse> deleteAnswer(@PathVariable("answerId") final String answerId, @RequestHeader("authorization") final String authorization) throws AuthorizationFailedException, AnswerNotFoundException {
@@ -60,6 +60,7 @@ public class AnswerController {
 
         return new ResponseEntity<AnswerDeleteResponse>(answerDeleteResponse, HttpStatus.OK);
     }
+    
     //This endpoint is used to get all answers to a particular question. Any user can access this endpoint.
     @RequestMapping(path = "answer/all/{questionId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<List<AnswerDetailsResponse>> getAllAnswersToQuestion(@PathVariable("questionId") final String questionId, @RequestHeader("authorization") final String authorization) throws AuthorizationFailedException, InvalidQuestionException {
